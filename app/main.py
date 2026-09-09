491bc808e0e88669e6bfbaa7dfe7df18a9465b45
Maksim Vecherski <maksvech812@gmail.com>
2026-09-09 21:25:58 +0300

    test env

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 import asyncio
 from app.agent.worker import bot, dp
+from app.settings import settings
 
 
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    print("Starting bot...")
+    print(f"BOT_TOKEN: {settings.BOT_TOKEN}")
+    print(f"MODEL_TOKEN: {settings.OPEN_AI_KEY}")
     asyncio.run(main())
